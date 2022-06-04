aafc17f3cef5a5ae847211231126e80685b3c49f
raeganfaith <57030395+raeganfaith@users.noreply.github.com>
2022-06-04 14:25:38 +0800

    map page update

---
+++ script.js
@@ -0,0 +1,52 @@
+// Navigation Hamburger menu
+
+// Toggle function of the View Task and Map
+// The Skeld toggle button
+function SkeldViewTasks() {
+  document.getElementById("div1").style.display="none";
+  document.getElementById("div2").style.display="block";
+}
+function SkeldViewMap() {
+  document.getElementById("div1").style.display="block";
+  document.getElementById("div2").style.display="none";
+}
+// The Airship Toggle Button
+function AirshipViewTasks() {
+  document.getElementById("div3").style.display="none";
+  document.getElementById("div4").style.display="block";
+}
+function AirshipViewMap() {
+  document.getElementById("div3").style.display="block";
+  document.getElementById("div4").style.display="none";
+}
+
+// Polus Toggle Button
+function PolusViewTasks() {
+  document.getElementById("div5").style.display="none";
+  document.getElementById("div6").style.display="block";
+}
+function PolusViewMap() {
+  document.getElementById("div5").style.display="block";
+  document.getElementById("div6").style.display="none";
+}
+
+// Map 5 Toggle Button
+function Map5ViewTasks() {
+  document.getElementById("div7").style.display="none";
+  document.getElementById("div8").style.display="block";
+}
+function Map5ViewMap() {
+  document.getElementById("div7").style.display="block";
+  document.getElementById("div8").style.display="none";
+}
+
+// Mira HQ Toggle Button
+function MiraViewTasks() {
+  document.getElementById("div9").style.display="none";
+  document.getElementById("div10").style.display="block";
+}
+function MiraViewMap() {
+  document.getElementById("div9").style.display="block";
+  document.getElementById("div10").style.display="none";
+}
+
